3877fad63eac8c255262ccc30735a41792d0f9d0
aibek shamshiev <aibek@MacBook-Air-aibek.local>
2022-02-10 16:54:17 +0600

    added signUp

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6eea6a4913bc5dc3"; }
+/******/ 		__webpack_require__.h = function() { return "48978d469161ce69"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- utils/axios.js
+++ utils/axios.js
@@ -25,31 +25,4 @@ $api.interceptors.request.use(
   }
 );
 
-$api.interceptors.response.use(
-  (config) => {
-    return config;
-  },
-  async (error) => {
-    const originalRequest = error.config;
-    if (
-      error.response.status == 401 &&
-      error.config &&
-      !error.config._isRetry
-    ) {
-      originalRequest._isRetry = true;
-      try {
-        const response = await $api.post(
-          '/users/auth/jwt/refresh/',
-          localStorage.getItem('refreshToken')
-        );
-        localStorage.setItem('accessToken', response.data.access);
-        return $api.request(originalRequest);
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    }
-    throw error;
-  }
-);
-
 export default $api;
