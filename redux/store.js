55b1a9cedf7b4d18636e7a99e97521af21477643
tarieltairov <tarieltairov1@gmail.com>
2022-02-10 17:38:34 +0600

    create autorization without logic

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./autorization/autorization.slice"
+
 const rootReducer = combineReducers({
     auth: authSlice
 })
