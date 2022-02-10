71212cfcbc07fa25b1e032fc3a65d0d4b61cb8d9
aibek shamshiev <aibek@MacBook-Air-aibek.local>
2022-02-10 13:59:22 +0600

    sosi

--- pages/registration.jsx
+++ pages/registration.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Registration from '../components/Registration/Registration';
 
 function registration() {
   return (
     <>
       <Registration />
+      <Footer />
     </>
   );
 }
