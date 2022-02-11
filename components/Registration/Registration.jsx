66bab094bc3b9deaf7e5b775c2cf563332d35135
aibek shamshiev <aibek@MacBook-Air-aibek.local>
2022-02-11 14:37:56 +0600

    asd

--- components/Registration/Registration.jsx
+++ components/Registration/Registration.jsx
@@ -8,17 +8,17 @@ const initialValues = {
   last_name: '',
 };
 
-const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
-});
+// const SignupSchema = Yup.object().shape({
+//   firstName: Yup.string()
+//     .min(2, 'Too Short!')
+//     .max(50, 'Too Long!')
+//     .required('Required'),
+//   lastName: Yup.string()
+//     .min(2, 'Too Short!')
+//     .max(50, 'Too Long!')
+//     .required('Required'),
+//   email: Yup.string().email('Invalid email').required('Required'),
+// });
 
 function Registration() {
   const handleSubmit = (values) => {
