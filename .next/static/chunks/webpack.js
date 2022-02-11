f5df7d637497b907eb559fdb073644d37d82ca20
aibek shamshiev <aibek@MacBook-Air-aibek.local>
2022-02-11 14:48:04 +0600

    asd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b6d3d91072a5374"; }
+/******/ 		__webpack_require__.h = function() { return "ff6e18886ce0bc0f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
