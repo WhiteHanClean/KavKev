54f96505b5a93b1588829a4bb72eb4a601de2118
WhiteHanClean <whitehanclean@gmail.com>
2022-02-15 09:20:11 +0300

    add_Cart

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ae7ffae71e72bc66"; }
+/******/ 		__webpack_require__.h = function() { return "84f7fc40335a03bf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
