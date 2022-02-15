1374ed0fe6dcbecc75a891164f24e57fde59ad6d
WhiteHanClean <whitehanclean@gmail.com>
2022-02-15 10:43:39 +0300

    ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a504aedb640f21c6"; }
+/******/ 		__webpack_require__.h = function() { return "3867bd39d540ace7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
