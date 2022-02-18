ff1f61d196ceb9bc7c94bf5baf75c532e7f85111
Baiaman Kodirov <baiamankodirov@MacBook-Pro-Baiaman.local>
2022-02-18 13:55:15 +0600

    header cart ichezaet v route cart

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "54365abc12f7d7e9"; }
+/******/ 		__webpack_require__.h = function() { return "e2761009ceab1cdc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
