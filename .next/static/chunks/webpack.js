6f04ab1960ed35705c5244fb44e2a9796fd9e360
Baiaman Kodirov <baiamankodirov@MacBook-Pro-Baiaman.local>
2022-02-18 16:18:18 +0600

    merge main and abror

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ed2cab913fb35fad"; }
+/******/ 		__webpack_require__.h = function() { return "ed110a0779b5be42"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
